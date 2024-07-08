753cd0e45a48cbdd9d26ca8a01f19a7681ef4c67
can aydin <canaydindev@gmail.com>
2024-07-08 17:11:03 +0300

    last update

---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  Input,
+  
+} from "@nextui-org/react";
+import HeaderAuth from '@/components/header-auth';
+
+export default function Header() {
+  
+
+  
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Home
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <Input />
+      </NavbarContent>
+      <NavbarContent justify="end"><HeaderAuth/></NavbarContent>
+    </Navbar>
+  );
+}
